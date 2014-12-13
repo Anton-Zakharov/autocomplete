bde5d4f9e4f1fc1cdf72831f7a3ca7caf0ab566a
Anton Zakharov <anton-zakharov@mail.ru>
2014-12-13 11:17:40 +0500

    Добавлены пустые проекты для серверной и клиентской части приложения.

---
+++ Zakharov.Autocomplete.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zakharov.Autocomplete.Client {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
